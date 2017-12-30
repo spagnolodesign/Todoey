f99c0e3be91518d63f3c4800aae23bb993d42d86
spagnolodesign <luca.spagnolo91@gmail.com>
2017-12-30 20:56:05 +0100

    Set Up Realm DB

--- Todoey/Controllers/AppDelegate.swift
+++ Todoey/Controllers/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-                
+        
         let dog = Dog()
         dog.name = "Rex"
         dog.age = 1
